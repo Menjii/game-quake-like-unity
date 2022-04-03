2f0f462a590470466de004539783e5270f3d1b73
Bartosz <bartoszw8@gmail.com>
2022-04-03 21:05:42 +0200

    Added functional sliding-doors

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Door Door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<CharacterController>(out CharacterController controller))
+        {
+            if (!Door.IsOpen)
+            {
+                Door.Open();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<CharacterController>(out CharacterController controller))
+        {
+            if (Door.IsOpen)
+            {
+                Door.Close();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Door.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool IsOpen = false;
+    [SerializeField]
+    private float Speed = 1f;
+
+    [Header("Sliding Configs")]
+    [SerializeField]
+    private Vector3 SlideDirection = Vector3.back;
+    [SerializeField]
+    private float SlideAmount = 7.0f;
+
+    private Vector3 StartPosition;
+
+    private Coroutine AnimationCoroutine;
+
+    private void Awake()
+    {
+        StartPosition = transform.position;
+    }
+
+    public void Open()
+    {
+        if (!IsOpen)
+        {
+            if (AnimationCoroutine != null)
+            {
+                StopCoroutine(AnimationCoroutine);
+            }
+            AnimationCoroutine = StartCoroutine(DoSlidingOpen());
+        }
+    }
+
+    private IEnumerator DoSlidingOpen()
+    {
+        Vector3 endPosition = StartPosition + SlideAmount * SlideDirection;
+        Vector3 startPosition = transform.position;
+
+        float time = 0;
+        IsOpen = true;
+        while (time < 1)
+        {
+            transform.position = Vector3.Lerp(startPosition, endPosition, time);
+            yield return null;
+            time += Time.deltaTime * Speed;
+        }
+    }
+
+    public void Close()
+    {
+        if (IsOpen)
+        {
+            if (AnimationCoroutine != null)
+            {
+                StopCoroutine(AnimationCoroutine);
+            }
+
+            AnimationCoroutine = StartCoroutine(DoSlidingClose());
+        }
+    }
+
+    private IEnumerator DoSlidingClose()
+    {
+        Vector3 endPosition = StartPosition;
+        Vector3 startPosition = transform.position;
+        float time = 0;
+
+        IsOpen = false;
+
+        while (time < 1)
+        {
+            transform.position = Vector3.Lerp(startPosition, endPosition, time);
+            yield return null;
+            time += Time.deltaTime * Speed;
+        }
+    }
+}
